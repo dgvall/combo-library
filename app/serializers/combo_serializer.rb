40d59eb4e887d39c52811aaa5abfcd5308e7c127
Diego Valles <dgvall134@gmail.com>
2023-05-03 21:33:30 -0600

    Will serialize Combo attribute inputs to instead send an array of cooresponding image urls

--- app/serializers/combo_serializer.rb
+++ app/serializers/combo_serializer.rb
@@ -1,3 +1,20 @@
 class ComboSerializer < ActiveModel::Serializer
-  attributes :id, :inputs, :youtube_id, :starter, :location, :hit_type, :meterless
+  attributes :id, :image_urls, :youtube_id, :starter, :location, :hit_type, :meterless
+
+  def image_urls
+    object.inputs.split(' ').map do |input|
+      case input
+      when '-'
+        'https://i.imgur.com/SzG00rs.png'
+      when 'S'
+        'https://imgur.com/KtvFf0G'
+      when '2'
+        'https://imgur.com/KezFbHr'
+      when 'H'
+        'https://imgur.com/Ki3AR78'
+      when '236'
+        'https://imgur.com/FGoSIYq'
+      end
+    end
+  end
 end
