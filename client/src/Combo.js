131672a881d0ad85c29e6e1987213189b89c5f6a
Diego Valles <dgvall134@gmail.com>
2023-05-03 23:53:20 -0600

    Will add relevant icons and styling to Combo component

--- client/src/Combo.js
+++ client/src/Combo.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import YouTube from 'react-youtube'
 
 import Icon from './Icon'
@@ -6,10 +6,23 @@ import Icon from './Icon'
 import './Combo.css'
 
 function Combo( { id, imageUrls, youtubeId} ) {
+  const [showVideo, setShowVideo] = useState(false)
+  const [showNotes, setShowNotes] = useState(false)
   console.log(imageUrls)
   return (
     <div className = 'combo-container'>
-      <h1>Combo</h1>
+      <div className = 'buttons-container'>
+        <button
+          onClick = {() => setShowNotes(() => !showNotes)}
+        >🗒️</button>
+
+        <button
+          onClick = {() => setShowVideo(() => !showVideo)}
+        >▶️</button>
+
+        
+      </div>
+
       <div className = 'icons-container'>
         {
           imageUrls.map((url, index) => {
@@ -22,7 +35,21 @@ function Combo( { id, imageUrls, youtubeId} ) {
           })
         }
       </div>
-      <YouTube videoId = {youtubeId}/>
+
+      {
+        showVideo &&
+        <YouTube videoId = {youtubeId}/>
+      }
+
+      {
+        showNotes &&
+        <p>Author notes will go here, add them to the schema soon! Emoji should only show if it includes notes. Cog should only show if user uploaded the combo!</p>
+      }
+
+      <div className = 'buttons-container'>
+        <button>⚙️</button>
+        <button>🔖</button>
+      </div>
     </div>
   )
 }
