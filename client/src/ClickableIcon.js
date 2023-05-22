0cb01ce4bc76ff0587327c84509ff242d44fc999
Diego Valles <dgvall134@gmail.com>
2023-05-21 22:37:21 -0600

    Will add new universal icons to combo builder and refactor direction icons

--- client/src/ClickableIcon.js
+++ client/src/ClickableIcon.js
@@ -2,15 +2,26 @@ import React from 'react'
 
 import './Icon.css'
 
-function ClickableIcon( {url, name, handleClick} ) {
+function ClickableIcon( {url, name, handleClick, spaceIcon} ) {
   return (
     <div>
-      <img
-        onClick = {() => handleClick(url, name)}
-        className = 'icon'
-        src = {url}
-        alt = {name}
-      />
+      {
+        !spaceIcon
+        ?
+        <img
+          onClick = {() => handleClick(url, name)}
+          className = 'icon'
+          src = {url}
+          alt = {name}
+        />
+        :
+        <img
+          onClick = {() => handleClick(url, name)}
+          className = 'spaced-icon'
+          src = {url}
+          alt = {name}
+        />
+      }
     </div>
   )
 }
--- client/src/IconPicker.js
+++ client/src/IconPicker.js
@@ -6,7 +6,7 @@ import ClickableIcon from './ClickableIcon'
 
 function IconPicker({extraInputs, buttonInputs, motionInputs, handleClick, handleSpace}) {
 
-  const directionInputs = [
+  const directionsRow1 = [
     {
       name: "7",
       image_url: "https://i.imgur.com/2rO9chH.png"
@@ -19,6 +19,9 @@ function IconPicker({extraInputs, buttonInputs, motionInputs, handleClick, handl
       name: "9",
       image_url: "https://i.imgur.com/UqYEenq.png"
     },
+  ]
+
+  const directionsRow2 = [
     {
       name: "4",
       image_url: "https://i.imgur.com/HwKpXDr.png"
@@ -31,6 +34,9 @@ function IconPicker({extraInputs, buttonInputs, motionInputs, handleClick, handl
       name: "6",
       image_url: "https://i.imgur.com/gyfnDrv.png"
     },
+  ]
+
+  const directionsRow3 = [
     {
       name: "1",
       image_url: "https://i.imgur.com/Fby15hF.png"
@@ -43,6 +49,92 @@ function IconPicker({extraInputs, buttonInputs, motionInputs, handleClick, handl
       name: "3",
       image_url: "https://i.imgur.com/06D3AyK.png"
     }
+  ]
+
+  // const directionInputs = [
+  //   {
+  //     name: "7",
+  //     image_url: "https://i.imgur.com/2rO9chH.png"
+  //   },
+  //   {
+  //     name: "8",
+  //     image_url: "https://i.imgur.com/8fY51s7.png"
+  //   },
+  //   {
+  //     name: "9",
+  //     image_url: "https://i.imgur.com/UqYEenq.png"
+  //   },
+  //   {
+  //     name: "4",
+  //     image_url: "https://i.imgur.com/HwKpXDr.png"
+  //   },
+  //   {
+  //     name: "5",
+  //     image_url: "https://i.imgur.com/OfO6HKV.png"
+  //   },
+  //   {
+  //     name: "6",
+  //     image_url: "https://i.imgur.com/gyfnDrv.png"
+  //   },
+  //   {
+  //     name: "1",
+  //     image_url: "https://i.imgur.com/Fby15hF.png"
+  //   },
+  //   {
+  //     name: "2",
+  //     image_url: "https://i.imgur.com/KezFbHr.png"
+  //   },
+  //   {
+  //     name: "3",
+  //     image_url: "https://i.imgur.com/06D3AyK.png"
+  //   }
+  // ]
+
+  const universalInputs = [
+    {
+      name: "[",
+      image_url: "https://i.imgur.com/rXmq6Ps.png"
+    },
+    {
+      name: "]",
+      image_url: "https://i.imgur.com/HgDolUR.png"
+    },
+    {
+      name: "j.",
+      image_url: "https://i.imgur.com/X4u3eOL.png"
+    },
+    {
+      name: "jc.",
+      image_url: "https://i.imgur.com/gnDR9rY.png"
+    },
+    {
+      name: "dj.",
+      image_url: "https://i.imgur.com/zTALYJv.png"
+    },
+    {
+      name: "sj.",
+      image_url: "https://i.imgur.com/o8VUuQr.png"
+    },
+    {
+      name: "dl.",
+      image_url: "https://i.imgur.com/3Yiy3t1.png"
+    },
+    {
+      name: "md.",
+      image_url: "https://i.imgur.com/e92KV5m.png"
+    },
+    {
+      name: "tk.",
+      image_url: "https://i.imgur.com/dDhnIA9.png"
+    },
+    {
+      name: "land",
+      image_url: "https://i.imgur.com/KTDY9lc.png"
+    },
+    {
+      name: "OTG",
+      image_url: "https://i.imgur.com/hE5DELK.png"
+    },
   ]
   return (
     <div className = 'picker-container'>
@@ -78,23 +170,70 @@ function IconPicker({extraInputs, buttonInputs, motionInputs, handleClick, handl
               })
             }
           </div>
-        </div>
-
-        <div className = 'picker-column'>
-          <h2>Directions</h2>
-          <div className = 'picker-row-directions'>
+          <h2>Universal</h2>
+          <div className = 'picker-row'>
             {
-              directionInputs.map((u, index) => {
+              universalInputs.map((u, index) => {
                 return (
                   <ClickableIcon
                     handleClick = {handleClick}
-                    key = {index}
+                    key = {u.id}
                     url = {u.image_url}
                     name = {u.name}
+                    spaceIcon = {true}
                   />
                 )
               })
             }
+          </div>
+        </div>
+
+        <div className = 'picker-column'>
+          <h2>Directions</h2>
+          <div className = 'picker-directions'>
+            <div className = 'picker-row'>
+              {
+                directionsRow1.map((u, index) => {
+                  return (
+                    <ClickableIcon
+                      handleClick = {handleClick}
+                      key = {u.name}
+                      url = {u.image_url}
+                      name = {u.name}
+                    />
+                  )
+                })
+              }
+            </div>
+            <div className = 'picker-row'>
+              {
+                directionsRow2.map((u, index) => {
+                  return (
+                    <ClickableIcon
+                      handleClick = {handleClick}
+                      key = {u.name}
+                      url = {u.image_url}
+                      name = {u.name}
+                    />
+                  )
+                })
+              }
+            </div>
+            <div className = 'picker-row'>
+              {
+                directionsRow3.map((u, index) => {
+                  return (
+                    <ClickableIcon
+                      handleClick = {handleClick}
+                      key = {u.name}
+                      url = {u.image_url}
+                      name = {u.name}
+                    />
+                  )
+                })
+              }
+            </div>
+            
           </div>  
           <h2>Motions</h2>
           <div className = 'picker-row'>
