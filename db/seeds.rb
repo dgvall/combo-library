4673f80f4d7b183074b15d95c33836ca0abcffcb
Diego Valles <dgvall134@gmail.com>
2023-05-22 19:41:24 -0600

    Will add seeded data for sf6 characters

--- db/seeds.rb
+++ db/seeds.rb
@@ -387,18 +387,147 @@
 
 # sf6.inputs << [lp, mp, hp, lk, mk, hk, di, dr]
 
+#   blanka = Character.create!(
+#     name: "Blanka",
+#     slug: "blanka",
+#     image_url: "https://i.imgur.com/1t9AHLR.png",
+#     starters: "Drive Impact_Drive Rush_LP_MP_HP_LK_MK_HK_2LP_2MP_2HP_2LK_2MK_2HK_6MP_6MK_4MK_6HP_3HP_2PP_2PP>P_2PP>K_6KKK_4KKK_Electric Thunder_Rolling Attack_Vertical Rolling Attack_Backstep Rolling Attack_Aerial Rolling Attack_Wild Hunt_Blanka-chan Bomb_Rolling Cannon_Shout of Earth_Lightning Beast_Ground Shave Cannonball",
+#     game_id: 2
+#     )
+
+#   cammy = Character.create!(
+#     name: "Cammy",
+#     slug: "cammy",
+#     image_url: "https://i.imgur.com/PSeZzFw.png",
+#     starters: "Drive Impact_Drive Rush_LP_MP_HP_LK_MK_HK_2LP_2MP_2HP_2LK_2MK_2HK_4MP_6HK_4HK_4MP>HK_HP>HK_Spiral Arrow_Cannon Spike_Quick Spin Knuckle_Cannon Strike_Hooligan Combination_Razor's Edge Slicer_Cannon Strike_Cannon Strike_Fatal Leg Twister_Silent Step_Spin Drive Smasher_Killer Bee Spin_Aerial Killer Bee Spin_Delta Red Assault",
+#     game_id: 2
+#   )
+
+#   chun = Character.create!(
+#     name: "Chun-Li",
+#     slug: "chun-li",
+#     image_url: "https://i.imgur.com/B1GnLYm.png",
+#     starters: "Drive Impact_Drive Rush_LP_MP_HP_LK_MK_HK_2LP_2MP_2HP_2LK_2MK_2HK_4MP_6MP_4HP_3HP_6HK_3HK_6HP_j.2MK_j.9_j.HP>HP_214P_214P>LP_214P>MP_214P>HP_214P>LK_214P>MK_214P>HK_Kikoken_Hundred Lightning Kicks_Lightning Kick Barrage_Aerial Hundred Lightning Kicks_Spinning Bird Kick_Hazanshu_Tensho Kicks_Kikosho_Aerial Kikosho_Hoyoku-sen_Soten Ranka",
+#     game_id: 2
+#   )
+
+#   dee = Character.create!(
+#     name: "Dee Jay",
+#     slug: "dee-jay",
+#     image_url: "https://i.imgur.com/UWAo5GZ.png",
+#     starters: "Drive Impact_Drive Rush_LP_MP_HP_LK_MK_HK_2LP_2MP_2HP_2LK_2MK_2HK_j.2LK_6MK_4HK_MP>HP>HK_MP>MP>HP_MP>MP>4HP_j.MP>HP_3HP_22PP_2PP>P_2PP>K_6KKK_4KKK_Air Slasher_Jackknife Maximum_Roll Through Faint_Quick Rolling Sobat_Double Rolling Sobat_Machinegun Upper_Jus Cool_Funky Slicer_Waning Moon_Maximum Strike_Juggling Dash_Juggling Sway_The Greatest Sobat_Lowkey Sunrise Festival_Marvelous Sunrise Festival_Headliner Sunrise Festival_Climactic Strike_Encore Beat_Weekend Pleasure",
+#     game_id: 2
+#   )
+
+#   dhalsim = Character.create!(
+#     name: "Dhalsim",
+#     slug: "dhalsim",
+#     image_url: "https://i.imgur.com/akiT3TM.png",
+#     starters: "Drive Impact_Drive Rush_LP_MP_HP_LK_MK_HK_2LP_2MP_2HP_2LK_2MK_2HK_4MP_4HP_1HP_1LK_4MK_1MK_4HK_1HK_j.2LP_j.2K_Yoga Fire_Yoga Arch_Yoga Flame_Yoga Blast_Yoga Comet_Yoga Float_Aerial Yoga Float_Yoga Teleport_Aerial Yoga Teleport_Yoga Inferno_Yoga Sunburst_Merciless Yoga",
+#     game_id: 2
+#   )
+
+#   ehonda = Character.create!(
+#     name: "E. Honda",
+#     slug: "honda",
+#     image_url: "https://i.imgur.com/DciCrZV.png",
+#     starters: "Drive Impact_Drive Rush_LP_MP_HP_LK_MK_HK_2LP_2MP_2HP_2LK_2MK_2HK_6HK_3HK_j.2MK_LPMP_MP>3HK_Hundred Hand Slap_Sumo Headbutt_Sumo Smash_Oicho Throw_Sumo Dash_Teppo Triple Slap_Taiho Cannon Lift_Neko Damashi_Sumo Spirit_Show of Force_Ultimate Killer Head Ram_The Final Bout",
+#     game_id: 2
+#   )
+
+#   guile = Character.create!(
+#     name: "Guile",
+#     slug: "guile",
+#     image_url: "https://i.imgur.com/AtD2G2k.png",
+#     starters: "Drive Impact_Drive Rush_LP_MP_HP_LK_MK_HK_2LP_2MP_2HP_2LK_2MK_2HK_6MP_4HK_6HP_4LK_4MK_6MK_6HK_3HK_MP>4HP_MPMP_2MK>6MP_2HK>3HK_Sonic Boom_Somersault Kick_Sonic Blade_Sonic Cross_Sonic Break_Sonic Hurricane_Solid Puncher_Crossfire Somersault",
+#     game_id: 2
+#   )
+
+#   jaime = Character.create!(
+#     name: "Jaime",
+#     slug: "jaime",
+#     image_url: "https://i.imgur.com/mKQMSku.png",
+#     starters: "Drive Impact_Drive Rush_LP_MP_HP_LK_MK_HK_2LP_2MP_2HP_2LK_2MK_2HK_2KK_2HK>HK>P_6MK_4HP_6HK_LP>LK>MP_6MK>MK>P_4HP>HP>HK_6HK>4HK>P_The Devil Inside_Freeflow Strikes_Freeflow Kicks_Swagger Step_Arrow Kick_Luminous Dive Kick_Bakkai_Tenshin_Swagger Hermit Punch_Breakin'_The Devil's Song_Getsuga Saiho",
+#     game_id: 2
+#   )
+
+#   jp = Character.create!(
+#     name: "JP",
+#     slug: "jp",
+#     image_url: "https://i.imgur.com/cAWvXhB.png",
+#     starters: "Drive Impact_Drive Rush_LP_MP_HP_LK_MK_HK_2LP_2MP_2HP_2LK_2MK_2HK_6MK_3HP_6HK_4MP>MP_HK>HP_HK>HP>HP_HK>HP>HK_Triglav_Stribog_Departure_Depature: Window_Depature: Shadow_Amnesia_Torbalan_Chornobog_Lovushka_Interdiction",
+#     game_id: 2
+#   )
+
+#   juri = Character.create!(
+#     name: "Juri",
+#     slug: "juri",
+#     image_url: "https://i.imgur.com/rJp5Wtf.png",
+#     starters: "Drive Impact_Drive Rush_LP_MP_HP_LK_MK_HK_2LP_2MP_2HP_2LK_2MK_2HK_6MP_6MK_6HP_MP>4HP>6HP_Fuhajin_Saihasho_Ankensatsu_Go Ohsatsu_Tensenrin_Shiku-sen_Sakkai Fuhazan_Feng Shui Engine_Kaisen Dankai Raku",
+#     game_id: 2
+#   )
+
+#   ken = Character.create!(
+#     name: "Ken",
+#     slug: "ken",
+#     image_url: "https://i.imgur.com/dfB2ry2.png",
+#     starters: "Drive Impact_Drive Rush_LP_MP_HP_LK_MK_HK_2LP_2MP_2HP_2LK_2MK_2HK_KK_KK>LK_KK>MK_KK>HK_MP>HP_MK>MK>HK_Hadoken_Shoryuken_Tatsumaki Senpu-kyaku_Aerial Tatsumaki Senpukyaku_Dragonlash Kick_Jinrai Kick_Kazekama Shin Kick_Gorai Axe Kick_Senka Snap Kick_Kasai Thrust Kick_Dragonlash Flame_Shippu Jinrai-kyaku_Shinryu Reppa",
+#     game_id: 2
+#   )
+
+#   kimberly = Character.create!(
+#     name: "Kimberly",
+#     slug: "kimberly",
+#     image_url: "https://i.imgur.com/yIlMeHu.png",
+#     starters: "Drive Impact_Drive Rush_LP_MP_HP_LK_MK_HK_2LP_2MP_2HP_2LK_2MK_2HK_3MK_4HK_6HK_j.2MP_MP>HP_LP>MP>HP>HK_LP>MP>2HP>HK_LP>MP>2HP>2HK_Bushin Sepukyaku_Aerial Bushin Sepukyaku_Sprint_Emergency Stop_Torso Cleaver_Shadow Slide_Neck Hunter_Arc Step_Bushin Izuna Otoshi_Bushin Hojin Kick_Vagabond Edge_Hidden Variable_Genius at Play_Shuriken Bomb_Shuriken Bomb Sprea_Nue Twister_Bushin Beats_Bushin Thunderous Beats_Bushin Scramble_Soaring Bushin Scramble_Bushin Ninjastar Cypher",
+#     game_id: 2
+#   )
+
+#   lily = Character.create!(
+#     name: "Lily",
+#     slug: "lily",
+#     image_url: "https://i.imgur.com/BXVVTNF.png",
+#     starters: "Drive Impact_Drive Rush_LP_MP_HP_LK_MK_HK_2LP_2MP_2HP_2LK_2MK_2HK_3HP_4HP_j.2HP_j.MP>MP_6HP>HP>HP_Condor Wind_Condor Spire_Tomahawk Buster_Condor Dive_OD Condor Dive_Mexican Typhoon_Breezing Hawk_Thunderbird_Soaring Thunderbird_Raging Typhoon",
+#     game_id: 2
+#   )
+
 #  luke = Character.create!(
 #   name: "Luke",
 #   slug: "luke",
-#   image_url: "https://www.streetfighter.com/6/assets/images/character/luke/luke.png",
-#   starters: "LP MP HP LK MK HK 2LP 2MP 2HP 2LK 2MK 2HK 6MP 236P 214P 214M 214H 236236K 236236P 214214P",
+#   image_url: "https://i.imgur.com/xXrROHU.png",
+#   starters: "Drive Impact_Drive Rush_LP_MP_HP_LK_MK_HK_2LP_2MP_2HP_2LK_2MK_2HK_6MP_4HP_4HK_6HP>6HP_LP>MP>HP_2MK>2HP_MP>MP>MP>MP_Sand Blast_Fatal Shot_Flash Knuckle_DDT_Aerial Flash Knuckle_Avenger_No Chaser_Impaler_Rising Uppercut_Slam Dunk_Vulcan Blast_Eraser_Pale Rider",
 #   game_id: 2
-#  )
+#  ) 
+
+#  manon = Character.create!(
+#     name: "Manon",
+#     slug: "manon",
+#     image_url: "https://i.imgur.com/5eFvkSL.png",
+#     starters: "Manon:
+#     Drive Impact_Drive Rush_LP_MP_HP_LK_MK_HK_2LP_2MP_2HP_2LK_2MK_2HK_4HP_3HK_MP>MK_4MK>MK_HP>HP_2HP>HP_Manège Doré_Rond-point_Dégagé_Renversé_Grand Fouretté_Arabesque_Étoile_Pas de Deux",
+#     game_id: 2
+#   )
+
+#  marisa = Character.create!(
+#     name: "Marisa",
+#     slug: "marisa",
+#     image_url: "https://i.imgur.com/5OyRfTM.png",
+#     starters: "Drive Impact_Drive Rush_LP_MP_HP_LK_MK_HK_2LP_2MP_2HP_2LK_2MK_2HK_[P]HP_[P]HK_LP>LP_MP>MP_HP>HP_j.MP>MP_j.2HP_4HP_6MP>HP_6MP>HK_3HP>3HP_6HK>6HK_Gladius_Dimachaerus_Phalanx_Quadriga_Scutum_Tonitrus_Procella_Enfold_Javelin of Marisa_Meteorite_Goddess of the Hunt",
+#     game_id: 2
+#   )
 
 #  ryu = Character.create!(
 #   name: "Ryu",
 #   slug: "ryu",
-#   image_url: "https://www.streetfighter.com/6/assets/images/character/ryu/ryu.png",
-#   starters: "LP MP HP LK MK HK 2LP 2MP 2HP 2LK 2MK 2HK 6MP 236P 214P 214M 214H 236236K 236236P 214214P",
+#   image_url: "https://i.imgur.com/NUDnnq9.png",
+#   starters: "Drive Impact_Drive Rush_LP_MP_HP_LK_MK_HK_2LP_2MP_2HP_2LK_2MK_2HK_6MP_6HP_4HP_4HK_6HK_HP>HK_MP>LK>HK_Hadoken_Shoryuken_Tatsumaki Senpu-kyaku_Aerial Tatsumaki Senpukyaku_High Blade Kick_Hashogeki_Denjin Charge_Shinku Hadoken_Shin Hageshi_Shin Shoryuken",
 #   game_id: 2
 #  )
+
+#  zangief = Character.create!(
+#     name: "Zangief",
+#     slug: "zangief",
+#     image_url: "https://i.imgur.com/RXcCav4.png",
+#     starters: "Drive Impact_Drive Rush_LP_MP_HP_LK_MK_HK_2LP_2MP_2HP_2LK_2MK_2HK_3MP_6MK_6HP_6HK_3HK_j.2HP_j.8HP_MP>MP>MP_22MK>MK>MK_Double Lariat_Screw Piledriver_Borscht Dynamite_Russian Suplex_Siberian Express_Tundra Storm_Aerial Russian Slam_Cyclone Lariat_Bolshoi Storm Buster",
+#     game_id: 2
+#   )
