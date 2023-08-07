0ec3e3ab9242b6eeac9f22ce5d5eff5de1deaca4
Diego Valles <dgvall134@gmail.com>
2023-08-07 01:04:44 -0600

    Will add more glossary items to glossary

--- client/src/Glossary.js
+++ client/src/Glossary.js
@@ -8,12 +8,68 @@ function Glossary() {
     <div className = 'glossary-page'>
       <GlossaryItem
         img = "https://i.imgur.com/VvCULEF.png"
-        description = "Represents moves that can only be executed in a sequence"
+        description = "Can only be executed in a sequence"
+      />
+      <div className = 'glossary-item-manual'>
+        <div className = 'glossary-icons-row'>
+          <img src = "https://i.imgur.com/rXmq6Ps.png"/>
+          <img src = "https://i.imgur.com/AREXTeF.png"/>
+          <img src = "https://i.imgur.com/HgDolUR.png"/>
+        </div>
+        <p>Hold a button</p>
+      </div>
+      <div className = 'glossary-item-manual'>
+        <div className = 'glossary-icons-row'>
+          <img src = "https://i.imgur.com/HgDolUR.png"/>
+          <img src = "https://i.imgur.com/AREXTeF.png"/>
+          <img src = "https://i.imgur.com/rXmq6Ps.png"/>
+        </div>
+        <p>Release a button</p>
+      </div>
+      <div className = 'glossary-item-manual'>
+        <div className = 'glossary-icons-row'>
+          <img src = "https://i.imgur.com/X4u3eOL.png"/>
+          <img src = "https://i.imgur.com/AREXTeF.png"/>
+        </div>
+        <p>Aerial / jumping attack</p>
+      </div>
+      <GlossaryItem
+        img = "https://i.imgur.com/gnDR9rY.png"
+        description = "Jump cancel"
       />
       <GlossaryItem
-        img = "e"
-        description = "hello"
+        img = "https://i.imgur.com/zTALYJv.png"
+        description = "Double jump"
       />
+      <GlossaryItem
+        img = "https://i.imgur.com/o8VUuQr.png"
+        description = "Super jump"
+      />
+       <GlossaryItem
+        img = "https://i.imgur.com/3Yiy3t1.png"
+        description = "Delay"
+      />
+       <GlossaryItem
+        img = "https://i.imgur.com/o8VUuQr.png"
+        description = "Super jump"
+      />
+      <GlossaryItem
+        img = "https://i.imgur.com/e92KV5m.png"
+        description = "Micro-dash"
+      />
+      <GlossaryItem
+        img = "https://i.imgur.com/dDhnIA9.png"
+        description = "(Tiger knee) Buffer input while rising"
+      />
+      <GlossaryItem
+        img = "https://i.imgur.com/KTDY9lc.png"
+        description = "Land from the air to the ground"
+      />
+      <GlossaryItem
+        img = "https://i.imgur.com/hE5DELK.png"
+        description = "(On the ground) Input after opponent has been knocked down"
+      />
+
       
     </div>
   )
