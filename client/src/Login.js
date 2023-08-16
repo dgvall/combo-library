bac244180a767ef577077b2f67bf416e0b52c156
Diego Valles <dgvall134@gmail.com>
2023-08-16 08:42:15 -0600

    Will change login/signup pages to feature private passwords

--- client/src/Login.js
+++ client/src/Login.js
@@ -53,6 +53,7 @@ function Login() {
       />
       <input
         className = 'form-input'
+        type= 'password'
         onChange = {(e) => setPassword(e.target.value)}
         value = {password}
         placeholder = "Password"
--- client/src/Signup.js
+++ client/src/Signup.js
@@ -56,12 +56,14 @@ function Signup() {
         />
         <input
           className = 'form-input'
+          type = "password"
           onChange = {(e) => setPassword(e.target.value)}
           value = {password}
           placeholder = "Password"
         />
         <input
           className = 'form-input'
+          type = "password"
           onChange = {(e) => setPasswordConfirmation(e.target.value)}
           value = {passwordConfirmation}
           placeholder = "Confirm Password"
